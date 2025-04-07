f878a7f3675a0998525a2fe67bb7e5fcb1a8e8b2
UrquizaN <urquiza-nobrega@outlook.com>
2025-04-06 21:22:29 -0300

    feat: implementation of tab key inside the markup text input

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -17,6 +17,18 @@ Hooks.UpdateLineNumbers = {
       lineNumbers.scrollTop = this.el.scrollTop
     })
 
+    this.el.addEventListener("keydown", (e) => {
+      if (e.key !== "Tab") return e;
+
+      e.preventDefault()
+
+      const start = this.el.selectionStart;
+      const end = this.el.selectionEnd;
+
+      this.el.value = this.el.value.substring(0, start) + "\t" + this.el.value.substring(end)
+      this.el.selectionStart = this.el.selectionEnd = start + 1
+    })
+
     this.handleEvent("clear-textarea", () => {
       this.el.value = ""
 
