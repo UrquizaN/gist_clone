4ffb8f5b477b0e075f413dc08479043e5defbe60
UrquizaN <urquiza-nobrega@outlook.com>
2025-04-12 16:45:43 -0300

    refactor: create gist to use gist_form_component

--- lib/gist_clone_web/live/create_gist_live.ex
+++ lib/gist_clone_web/live/create_gist_live.ex
@@ -5,36 +5,6 @@ defmodule GistCloneWeb.CreateGistLive do
   alias GistClone.Gists.Gist
 
   def mount(_params, _session, socket) do
-    form =
-      %Gist{}
-      |> Gists.change_gist()
-      |> to_form()
-
-    {:ok, assign(socket, form: form)}
-  end
-
-  def handle_event("validate", %{"gist" => gist_params}, socket) do
-    form =
-      %Gist{}
-      |> Gists.change_gist(gist_params)
-      |> Map.put(:action, :validate)
-      |> to_form()
-
-    {:noreply, assign(socket, form: form)}
-  end
-
-  def handle_event("create", %{"gist" => gist_params}, socket) do
-    case Gists.create_gist(socket.assigns.current_user, gist_params) do
-      {:ok, gist} ->
-        socket = push_event(socket, "clear-textarea", %{})
-
-        form = %Gist{} |> Gists.change_gist() |> to_form()
-        socket = assign(socket, form: form)
-
-        {:noreply, push_navigate(socket, to: ~p"/gist/#{gist.id}")}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, form: to_form(changeset))}
-    end
+    {:ok, socket}
   end
 end
---
+++ lib/gist_clone_web/live/gist_form_component.ex
@@ -0,0 +1,102 @@
+defmodule GistCloneWeb.GistFormComponent do
+  use GistCloneWeb, :live_component
+
+  alias GistClone.Gists
+  alias GistClone.Gists.Gist
+
+  def mount(socket) do
+    form =
+      %Gist{}
+      |> Gists.change_gist()
+      |> to_form()
+
+    {:ok, assign(socket, form: form)}
+  end
+
+  def handle_event("validate", %{"gist" => gist_params}, socket) do
+    form =
+      %Gist{}
+      |> Gists.change_gist(gist_params)
+      |> Map.put(:action, :validate)
+      |> to_form()
+
+    {:noreply, assign(socket, form: form)}
+  end
+
+  def handle_event("create", %{"gist" => gist_params}, socket) do
+    case Gists.create_gist(socket.assigns.current_user, gist_params) do
+      {:ok, gist} ->
+        socket = push_event(socket, "clear-textarea", %{})
+
+        form = %Gist{} |> Gists.change_gist() |> to_form()
+        socket = assign(socket, form: form)
+
+        {:noreply, push_navigate(socket, to: ~p"/gist/#{gist.id}")}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, form: to_form(changeset))}
+    end
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      <.form
+        for={@form}
+        class="flex flex-col gap-6"
+        phx-change="validate"
+        phx-submit="create"
+        phx-target={@myself}
+      >
+        <.input
+          field={@form[:description]}
+          placeholder="Gist description..."
+          class="mt-0 bg-dark/90 border border-gray-500 text-white"
+          phx-debounce="blur"
+        />
+
+        <div class="flex flex-col border border-gray-500 rounded-lg">
+          <div class=" p-3 bg-dark-400/50 rounded-t-lg">
+            <.input
+              field={@form[:name]}
+              placeholder="Filename including extension"
+              class="mt-0 bg-dark/90 border border-gray-500 text-white"
+              phx-debounce="blur"
+            />
+          </div>
+          <div
+            id="line-numbers-container"
+            class="grid grid-cols-[82px_1fr] w-full"
+            phx-update="ignore"
+          >
+            <textarea
+              id="line-numbers"
+              class="mt-1 min-h-52 text-right outline-none focus:ring-0 rounded-lg border-none bg-transparent text-gray-400 leading-3 resize-none overflow-hidden"
+              readonly
+              tabindex="-1"
+            >
+          <%= "1\n" %>
+          </textarea>
+            <.input
+              field={@form[:markup_text]}
+              placeholder="Enter your code..."
+              type="textarea"
+              class="flex-1 rounded-b-lg rounded-t-none resize-none bg-dark/30 border-transparent border-none text-white w-full h-full"
+              phx-debounce="blur"
+              phx-hook="UpdateLineNumbers"
+              id="markup-text"
+            />
+          </div>
+        </div>
+
+        <.button
+          class="bg-lavender-900 hover:bg-lavender w-32 self-end p-2 rounded-lg"
+          phx-disable-with="Creating..."
+        >
+          Create gist
+        </.button>
+      </.form>
+    </div>
+    """
+  end
+end
