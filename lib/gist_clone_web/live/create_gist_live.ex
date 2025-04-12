90b715685d08f6cffe29d41bdd00d18bd314f4ee
UrquizaN <urquiza-nobrega@outlook.com>
2025-04-12 18:10:01 -0300

    chore: remove unnused alias

--- lib/gist_clone_web/live/create_gist_live.ex
+++ lib/gist_clone_web/live/create_gist_live.ex
@@ -1,9 +1,6 @@
 defmodule GistCloneWeb.CreateGistLive do
   use GistCloneWeb, :live_view
 
-  alias GistClone.Gists
-  alias GistClone.Gists.Gist
-
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
